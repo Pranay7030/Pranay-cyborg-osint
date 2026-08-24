72ceece35a4318c7b32ec7f604d6dd6f79dcedb4
Pranay7030 <pm945797@gmail.com>
2026-08-25 01:49:37 +0530

    Add banner display function with rich console

---
+++ main.py
@@ -0,0 +1,13 @@
+from rich.console import Console
+console = Console()
+
+def show_banner(): # <-- 2nd wala SKULL code yaha
+    skull = r''' ... '''
+    console.print(skull...)
+
+def link_osint(): ...
+def insta_osint(): ...
+
+# last me
+show_banner()
+# menu code
